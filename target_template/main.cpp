16238314207e2e2d774f2585ad2fe5aaa9de9885
Davide Rizzi <d.rizzi@novalabs.io>
2017-05-22 15:05:13 +0200

    New target template

--- target_template/main.cpp
+++ target_template/main.cpp
@@ -1,128 +1,74 @@
+/* ADC Module template file
+ *
+ * ADCNode.cpp contains a sample node you can start from.
+ */
+
 #include <ModuleConfiguration.hpp>
 #include <Module.hpp>
 
-// MESSAGES
-#include <core/common_msgs/Led.hpp>
-#include <core/common_msgs/Float32.hpp>
-#include <core/mw/Publisher.hpp>
-
-// NODES
-#include <core/led/Subscriber.hpp>
-
-// BOARD IMPL
+// --- BOARD IMPL -------------------------------------------------------------
 #include <core/ADS1262_driver/ADS1262.hpp>
 
-// *** DO NOT MOVE ***
+// --- MODULE -----------------------------------------------------------------
 Module module;
 
-// TYPES
+// *** DO NOT MOVE THE CODE ABOVE THIS COMMENT *** //
 
-// NODES
-core::led::Subscriber led_subscriber("led_subscriber", core::os::Thread::PriorityEnum::LOWEST);
+// --- MESSAGES ---------------------------------------------------------------
+#include <core/common_msgs/Led.hpp>
 
-void
-adcThread(
-    void* args
-)
-{
-    using ADC = core::ADS1262_driver::ADS1262;
+// --- NODES ------------------------------------------------------------------
+#include <core/led/Subscriber.hpp>
+#include "ADCNode.hpp"
 
-    Module::adc.init();
-    Module::adc.probe();
-    Module::adc.configure();
+// --- TYPES ------------------------------------------------------------------
 
-    Module::adc.setReferenceMux(ADC::ReferenceMuxPositive::INTERNAL_2V5, ADC::ReferenceMuxNegative::INTERNAL_AVSS);
-    Module::adc.setReferencePolarityReversal(false);
+// --- CONFIGURATIONS ---------------------------------------------------------
+core::led::SubscriberConfiguration led_subscriber_configuration_default;
 
-    Module::adc.setInputMux(ADC::InputMuxPositive::AIN_0, ADC::InputMuxNegative::AIN_1);
+// --- NODES ------------------------------------------------------------------
+core::led::Subscriber led_subscriber("led_sub", core::os::Thread::PriorityEnum::LOWEST);
 
-    Module::adc.setPGA(false, ADC::Gain::GAIN_1);
+// --- DEVICE CONFIGURATION ---------------------------------------------------
 
-    Module::adc.setDataRate(ADC::DataRate::DATARATE_50);
-    Module::adc.setFilter(ADC::Filter::SINC_3);
-    Module::adc.setDelay(ADC::Delay::_278us);
-
-    Module::adc.start();
-    Module::adc.calibrateOffset();
-    Module::adc.stop();
-
-    int64_t _filteredValue = 0;
-    int32_t _cnt = 0;
-
-    core::mw::Node node("adc_thread");
-    core::mw::Publisher<core::common_msgs::Float32> publisher;
-
-    node.advertise(publisher, "adc0");
-
-    Module::adc.start();
-
-    while (1) {
-        Module::adc.wait(); // Wait for a new conversion
-        Module::adc.update();
-        auto status = Module::adc.getStatus();
-
-        status.pgal_alm = 0;
-
-        if (status & 0b01000000) {
-            // We have a new sample...
-            int32_t x = Module::adc.getRaw();
-
-            _filteredValue += x; // Accumulate the new sample
-
-            _cnt++;
-
-            if (_cnt == 50) {
-                _cnt = 0; // Reset the sample counter
-            }
-
-            if (_cnt == 0) {
-                // A whole perdiod has been accumulated
-                core::common_msgs::Float32* msgp;
-
-                float tmp = (float)(_filteredValue / 50) * (2.5f / 0x7FFFFFFF);  // Take the average
-                _filteredValue = 0; // Reset the accumulator
-
-                if (publisher.alloc(msgp)) {
-                    msgp->value = tmp;
-
-                    if (!publisher.publish(*msgp)) {}
-                }
-            }
-        }
-    }
-} // adcThread
-
-// MAIN
+// --- MAIN -------------------------------------------------------------------
 extern "C" {
     int
     main()
     {
         module.initialize();
 
-        // Led subscriber node
-        core::led::SubscriberConfiguration led_subscriber_configuration;
-        led_subscriber_configuration.topic = "led";
-        led_subscriber.setConfiguration(led_subscriber_configuration);
-        module.add(led_subscriber);
+        // Device configurations
 
-        // Setup and run
-        module.setup();
-        module.run();
+        // Default configuration
+        led_subscriber_configuration_default.topic = "led";
 
-        // ADC publisher
-        core::os::Thread::create_heap(NULL, 4096, core::os::Thread::PriorityEnum::NORMAL, adcThread, NULL);
+        // Add configurable objects to the configuration manager...
+        module.configurations().add(led_subscriber, led_subscriber_configuration_default);
+
+        // ... and load the configuration
+        module.configurations().loadFrom(module.configurationStorage());
+
+        // Add nodes to the node manager...
+        module.nodes().add(led_subscriber);
+
+        // ... and let's play!
+        module.nodes().setup();
+        module.nodes().run();
+
+        // Create a simple node
+        core::os::Thread::create_heap(NULL, 4096, core::os::Thread::PriorityEnum::NORMAL, adcNodeThread, NULL);
 
         // Is everything going well?
         for (;;) {
-            if (!module.isOk()) {
+            if (!module.nodes().areOk()) {
                 module.halt("This must not happen!");
             }
 
-#ifdef CORE_USE_BOOTLOADER
-            module.keepAlive();
-#endif
-
             core::os::Thread::sleep(core::os::Time::ms(500));
+
+            // Remember to feed the (watch)dog!
+            module.keepAlive();
         }
 
         return core::os::Thread::OK;
