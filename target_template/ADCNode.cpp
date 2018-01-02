f404c56de848a97a10c0be5bef24c84838e1bb96
Davide Rizzi <d.rizzi@novalabs.io>
2018-01-02 12:12:52 +0100

    Updated copyright

--- target_template/ADCNode.cpp
+++ target_template/ADCNode.cpp
@@ -1,3 +1,9 @@
+/* COPYRIGHT (c) 2016-2018 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+ 
 #include <Module.hpp>
 #include <core/ADS1262_driver/ADS1262.hpp>
 
